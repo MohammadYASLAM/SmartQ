4ea9223fbc05a883cccbb25835bdf2202e020512
MohammadYASLAM <145485048+MohammadYASLAM@users.noreply.github.com>
2025-04-28 06:44:58 +0300

    Delete old version from main branch

--- SmartQ/public/js/qrcode-generator.js
+++
@@ -1,9 +0,0 @@
-// qrcode-generator.js
-function generateQR(queueId) {
-    const qr = new QRCode(document.getElementById("qrcode"), {
-      text: `${window.location.origin}/user-queue.html?queueId=${queueId}`,
-      width: 200,
-      height: 200
-    });
-  }
-  
